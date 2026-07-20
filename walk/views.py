973ff71922834e413c1c7995bd46b741d3f041ab
saebom <saebom0111@naver.com>
2026-07-20 18:21:52 +0900

    feat: 웹소켓 라우팅 연결

--- walk/views.py
+++ walk/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
-from django.shortcuts import get_object_or_400
+from django.shortcuts import get_object_or_404
 from .models import WalkingSession, WalkingPath
 from geopy.distance import geodesic 
 

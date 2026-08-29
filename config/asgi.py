0b0556abe001cac6d404e25f2c6c5c856799379b
seoeunbi25 <seoeunbi25@naver.com>
2026-08-29 18:17:49 +0900

    build: Add Daphne and Nginx setup

--- config/asgi.py
+++ config/asgi.py
@@ -1,10 +1,15 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 import walk.routing
 from walk.middleware import JWTAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Hwaso Backend API',
